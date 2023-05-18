a1000b37b6df2b800f13988c497fb2d098508931
senkuu2322 <ibrahimariq11@upi.edu>
2023-05-18 19:40:11 +0700

    leaflet

--- application/views/layout/Viewhead.php
+++ application/views/layout/Viewhead.php
@@ -5,9 +5,9 @@
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <title>AdminLTE 3 | Top Navigation</title>
 <!-- Font Awesome Icons -->
-<link rel="stylesheet" href="<?=base_url()?>assets/template/plugins/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="<?=base_url()?>asset/plugins/fontawesome-free/css/all.min.css">
 <!-- Theme style -->
-<link rel="stylesheet" href="<?=base_url()?>assets/template/dist/css/adminlte.min.css">
+<link rel="stylesheet" href="<?=base_url()?>asset/dist/css/adminlte.min.css">
 <!-- Google Font: Source Sans Pro -->
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
@@ -19,6 +19,9 @@
 <!-- AdminLTE App -->
 <script src="../../dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="<?=base_url()?>assets/template/dist/js/demo.js"></script>
+<script src="<?=base_url()?>asset/dist/js/demo.js"></script>
+
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
 
 </head>
--- application/views/peta_leaflet.php
+++ application/views/peta_leaflet.php
@@ -1,1 +1,15 @@
-<h1>Hello Minasan, hajimemashite</h1>
+<div class="content">
+<div id="map" style="width: 100%; height: 530px; color:black;"></div>
+</div>
+<script>
+var map = L.map('map', {
+center: [-1.7912604466772375, 116.42311966554416],
+zoom: 5,
+layers:[]
+});
+var GoogleSatelliteHybrid= L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', {
+maxZoom: 22,
+attribution: 'Latihan Web GIS'
+}).addTo(map);
+</script>
+
